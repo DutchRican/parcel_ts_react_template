7e2d8bad5d53b22a39271083f97852157e523455
Paul van Woensel <pvanwoensel@gmail.com>
2021-05-21 14:53:05 -0500

    added local cert creation and https running

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const http = require('http');
+const https = require('https');
+const { readFileSync } = require('fs');
+const path = require('path');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const helmet = require('helmet');
 
@@ -15,9 +19,32 @@ app.use('/api', createProxyMiddleware(
     }
 ));
 
-app.use('/', express.static('dist'))
+app.use('/', express.static(path.join(__dirname, '..', '..', 'dist')))
 
 const port = Number(process.env.PORT || 8080);
-app.listen(port, () => {
-    console.log(`listening on port: ${port}`);
+const sslPort = Number(process.env.PORT || 4443);
+
+const httpServer = http.createServer(app);
+httpServer.listen(port, () => {
+    console.log(`🚀 listening on port: ${port}`);
 });
+
+let httpsServer
+try {
+    httpsServer = https.createServer({
+        key: readFileSync(path.join(__dirname, 'certs', 'server.key')),
+        cert: readFileSync(path.join(__dirname, 'certs', 'server.cert'))
+    }, app);
+    httpsServer.listen(sslPort, () => {
+        console.log(`🚀 listening on port: ${sslPort}`);
+    });
+    httpsServer.on('SIGINT', shutdown);
+} catch (e){
+    console.log('Could not start https server: ', e)
+}
+
+httpServer.on('SIGINT', shutdown);
+
+function shutdown(server) {
+    server.close(() => console.log('shutting down ..'));
+}
